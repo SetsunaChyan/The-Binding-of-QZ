7575cfeaf03f02e63576493364289dfd3c4192fe
oneman233 <1046459015@qq.com>
2019-07-04 23:13:20 +0800

    初始界面

---
+++ Assets/Scripts/Main_Page_Scripts.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Main_Page_Scripts : MonoBehaviour {
+
+    public Canvas Canvas;
+    public Button score_button,exit_button,hp_button;
+    public Text information;
+
+    int hp = 2;
+    int score = 0;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+    public void hp_minus()
+    {
+        Debug.Log("HP");
+        if (hp > 0)
+        {
+            hp--;
+            information.text = "血量：" + hp.ToString() + "分数：" + score.ToString();
+        }
+        if(hp == 0)
+        {
+            SceneManager.LoadScene("Dead_Page");
+        }
+    }
+
+    public void score_add()
+    {
+        Debug.Log("SCORE");
+        score++;
+        information.text = "血量：" + hp.ToString() + "分数：" + score.ToString();
+    }
+
+    public void exit()
+    {
+        Debug.Log("EXIT");
+        Application.Quit();
+    }
+}
---
+++ Assets/Scripts/exit_test.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class exit_test : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        this.GetComponent<Button>().onClick.AddListener(exit);
+	}
+
+    public void exit()
+    {
+        Debug.Log("退出了游戏！！！！！");
+        Application.Quit();
+    }
+}
