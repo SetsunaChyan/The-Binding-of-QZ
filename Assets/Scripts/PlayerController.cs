f9b93db0a9a8e93f0ad9e7884132947a4c5cd744
HiedanoAkyuu <ctzguozi@163.com>
2019-07-04 10:49:12 +0800

    Player

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour 
+{
+	//direction
+	private const int UP=0;
+	private const int LEFT=1;
+	private const int DOWN=2;
+	private const int RIGHT=3; 
+
+	private int currentHP;
+	private Rigidbody2D rigidbody2d;
+	private bool isInvincible;
+	private float invincibleTimer;
+	private int direction; //0:up 1:left 2:down 3:right
+
+	public float Speed;
+	public int MaxHP;
+	public int MinHP;
+
+	public int CurrentHP
+	{
+		get{ return currentHP;}
+		set{ currentHP=value;}
+	}
+
+	void Start() 
+	{
+		rigidbody2d=GetComponent<Rigidbody2D>();
+        currentHP=MaxHP;
+        isInvincible=false;
+	}
+	
+	void Update()
+	{
+		float horizontal=Input.GetAxis("Horizontal");
+        float vertical=Input.GetAxis("Vertical");
+
+		//更新位置
+        Vector2 position=rigidbody2d.position;
+        position.x+=Speed*horizontal*Time.deltaTime;
+        position.y+=Speed*vertical*Time.deltaTime;
+
+        rigidbody2d.MovePosition(position);
+
+		//更新朝向
+		Vector3 rotation=transform.localEulerAngles;
+		if(direction==0) rotation.z=0;
+		else if(direction) rotation.z=90;
+		else if(horizontal>0&&vertical<0) rotation.z=180;
+		if(horizontal<0&&vertical<0) rotation.z=270;
+		transform.localEulerAngles=rotation;
+
+		//更新无敌时间
+        if(isInvincible)
+        {
+            invincibleTimer-=Time.deltaTime;
+            if(invincibleTimer<0) isInvincible=false;
+        }
+	}
+}
---
+++ Document.md
@@ -0,0 +1,28 @@
+## PlayerController
+
+用来控制Player
+
+
+
+### Public方法
+
+##### CurrentHP.get()
+
+获取当前生命值
+
+##### CurrentHP.set()
+
+修改当前生命值
+
+
+
+### Public变量
+
+##### MaxHP
+
+最大生命值
+
+##### MinHP
+
+最小生命值，小于这个就会挂掉
+
